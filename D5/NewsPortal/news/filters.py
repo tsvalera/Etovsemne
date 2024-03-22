941c84dad5515728b3d36e8047bcd04f39395d8d
Valera <valera.tsagaraev@mail.ru>
2024-03-22 04:30:35 +0300

    D5

---
+++ D5/NewsPortal/news/filters.py
@@ -0,0 +1,21 @@
+from django_filters import FilterSet, DateTimeFilter, ModelMultipleChoiceFilter
+from django.forms import DateTimeInput
+from .models import Post
+
+
+class PostFilter(FilterSet):
+    added_after = DateTimeFilter(
+        field_name='dateCreation',
+        lookup_expr='gt',
+        widget=DateTimeInput(
+            format='%Y-%m-%dT%H:%M',
+            attrs={'type': 'datetime-local'},
+        ),
+    )
+
+    class Meta:
+        model = Post
+        fields = {
+            'title': ['icontains'],
+            'categoryType': ['exact'],
+        }
